15ae6d17aa631dc873104c47df57c809f76c08d0
Magnus Flor Johansen <magnus.johansen@bonnier.dk>
2018-01-10 14:35:15 +0100

    Refactor of JavaScript

--- src/Assets/Scripts.php
+++ src/Assets/Scripts.php
@@ -15,7 +15,10 @@ class Scripts
     {
         $script_src = trim(WpOAuth::instance()->getPluginUrl(), '/') . '/assets/js/bp-cl-oauth-login.js';
         wp_register_script('bp-wa-oauth-login', $script_src, [], '2.0.0', true);
-        wp_localize_script('bp-wa-oauth-login', 'translations', ['current_language' => pll__('Download Image')]);
+        wp_localize_script('bp-wa-oauth-login', 'routes', [
+            'login' => WpOAuth::instance()->getRoutes()->getLoginRoute(),
+            'logout' => WpOAuth::instance()->getRoutes()->getLogoutRoute()
+        ]);
         wp_enqueue_script('bp-wa-oauth-login');
     }
 }
