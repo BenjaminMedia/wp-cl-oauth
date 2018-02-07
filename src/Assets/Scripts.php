1d0d90acbb4061e934ddacdb239ad3ea9d54f231
Magnus Flor Johansen <magnus.johansen@bonnier.dk>
2018-02-07 16:12:51 +0100

    WILL-189 - Version bump

--- src/Assets/Scripts.php
+++ src/Assets/Scripts.php
@@ -14,7 +14,7 @@ class Scripts
     public static function enqueue_login_script()
     {
         $script_src = trim(WpOAuth::instance()->getPluginUrl(), '/') . '/assets/js/bp-cl-oauth-login.js';
-        wp_register_script('bp-wa-oauth-login', $script_src, [], '2.0.0', true);
+        wp_register_script('bp-wa-oauth-login', $script_src, [], '2.0.13', true);
         wp_localize_script('bp-wa-oauth-login', 'routes', [
             'login' => WpOAuth::instance()->getRoutes()->getLoginRoute(),
             'logout' => WpOAuth::instance()->getRoutes()->getLogoutRoute()
