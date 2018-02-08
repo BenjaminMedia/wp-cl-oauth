554d0e5917a9f15986def1926de9f39020e2da83
Jonas Kjaergaard <jonask@jonask.dk>
2018-02-08 12:31:37 +0100

    fixed right trim of domain-string

--- bp-cl-oauth.php
+++ bp-cl-oauth.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Bonnier OAuth Plugin
- * Version: 2.0.13
+ * Version: 2.0.14
  * Plugin URI: https://github.com/BenjaminMedia/wp-cl-oauth
  * Description: This plugin allows you to integrate your site with an OAuth2 service
  * Author: Bonnier
